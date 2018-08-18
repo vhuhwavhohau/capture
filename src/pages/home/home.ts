e19d8d42c6e1212ee765055302419a8aefae1987
vhuhwavhohau <mavhandu3@ymail.com>
2018-08-18 12:41:12 +0200

    first commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,66 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams,AlertController } from 'ionic-angular';
+import {Camera, CameraOptions} from '@ionic-native/camera';
+/**
+ * Generated class for the HomePage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html',
+})
+export class HomePage {
+  public photos : any;
+  public base64Image : string;
+  constructor(public navCtrl: NavController, public navParams: NavParams, private camera : Camera, private alertCtrl : AlertController) {
+  }
+  ngOnInit() {
+    this.photos = [];
+  }
+
+  deletePhoto(index){
+    let confirm = this.alertCtrl.create({
+      title: 'Sure you want to delete this photo? There is NO undo!',
+      message: '',
+      buttons: [
+        {
+          text: 'No',
+          handler: () => {
+            console.log('Disagree clicked');
+          }
+        }, {
+          text: 'Yes',
+          handler: () => {
+            console.log('Agree clicked');
+            this.photos.splice(index, 1);
+          }
+        }
+      ]
+    });
+  confirm.present();
+ }
+ 
+   takePhoto(){
+    const options : CameraOptions = {
+      quality: 50, // picture quality
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    this.camera.getPicture(options) .then((imageData) => {
+        this.base64Image = "data:image/jpeg;base64," + imageData;
+        this.photos.push(this.base64Image);
+        this.photos.reverse();
+      }, (err) => {
+        console.log(err);
+      });
+   }
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad HomePage');
+  }
+
+}
